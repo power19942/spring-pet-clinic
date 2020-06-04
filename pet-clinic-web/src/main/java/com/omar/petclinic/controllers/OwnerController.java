b26a4d4a259524bda8fd171c6469e19b5e320024
omar jakmira <poweromar19942@gmail.com>
2020-06-05 02:41:18 +0300

    adding VetController and OwnerController

---
+++ pet-clinic-web/src/main/java/com/omar/petclinic/controllers/OwnerController.java
@@ -0,0 +1,13 @@
+package com.omar.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/owners")
+@Controller
+public class OwnerController {
+    @RequestMapping("")
+    public String listOwners(){
+        return "owners/index";
+    }
+}
---
+++ pet-clinic-web/src/main/java/com/omar/petclinic/controllers/VetController.java
@@ -0,0 +1,13 @@
+package com.omar.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/vets")
+@Controller
+public class VetController {
+    @RequestMapping("")
+    public String listVets(){
+        return "vets/index";
+    }
+}
